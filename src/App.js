2b79d5b7cdba44ba011d35c7066eb061dcbc5ee9
Andrii Tynok <andriytinok@gmail.com>
2019-08-16 12:24:37 +0300

    5.21

--- src/App.js
+++ src/App.js
@@ -3,54 +3,61 @@ import axios from 'axios'
 
 
 const useField = (type) => {
-  const [value, setValue] = useState('')
+  const [value, setValue] = useState('');
 
   const onChange = (event) => {
     setValue(event.target.value)
-  }
+  };
 
   return {
     type,
     value,
     onChange
   }
-}
+};
 
 const useResource = (baseUrl) => {
-  const [resources, setResources] = useState([])
+  const [resources, setResources] = useState([]);
 
-  // ...
+  useEffect(() => {
+    axios.get(baseUrl).then(({data}) => {
+      setResources(data);
+    });
+  }, [baseUrl]);
 
   const create = (resource) => {
-    // ...
-  }
+    return axios.post(baseUrl, resource)
+        .then(({data}) => {
+          setResources([...resources, data]);
+        });
+  };
 
   const service = {
     create
-  }
+  };
 
   return [
     resources, service
   ]
-}
+};
 
 const App = () => {
-  const content = useField('text')
-  const name = useField('text')
-  const number = useField('text')
+  const content = useField('text');
+  const name = useField('text');
+  const number = useField('text');
 
-  const [notes, noteService] = useResource('http://localhost:3005/notes')
-  const [persons, personService] = useResource('http://localhost:3005/persons')
+  const [notes, noteService] = useResource('http://localhost:3005/notes');
+  const [persons, personService] = useResource('http://localhost:3005/persons');
 
   const handleNoteSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     noteService.create({ content: content.value })
-  }
+  };
  
   const handlePersonSubmit = (event) => {
-    event.preventDefault()
+    event.preventDefault();
     personService.create({ name: name.value, number: number.value})
-  }
+  };
 
   return (
     <div>
@@ -70,6 +77,6 @@ const App = () => {
       {persons.map(n => <p key={n.id}>{n.name} {n.number}</p>)}
     </div>
   )
-}
+};
 
 export default App
